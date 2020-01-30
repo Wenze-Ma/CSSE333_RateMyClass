98e8bf8a59256d6762806716f5252bdb1b0e29ea
Wenze Ma <maw1@rose-hulman.edu>
2020-01-30 15:22:10 -0500

    changed the frame

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,7 @@ import javax.swing.JFrame;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
-		//1. Create the frame.
-		JFrame frame = new JFrame("FrameDemo");
-
-		//2. Optional: What happens when the frame closes?
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		//3. Create components and put them in the frame.
-		//...create emptyLabel...
-
-		//4. Size the frame.
-		frame.pack();
-
-		//5. Show it.
-		frame.setVisible(true);
-		
+		new RateMyClassMain();
 	}
 
 }
---
+++ src/RateMyClassMain.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class RateMyClassMain {
+	private JFrame myFrame;
+	
+	
+	public RateMyClassMain() {
+		this.myFrame = new JFrame();
+		
+		myFrame.setSize(1500, 1000);
+		myFrame.setTitle("Rate My Class");
+		
+		myFrame.setVisible(true);
+	}
+
+}
