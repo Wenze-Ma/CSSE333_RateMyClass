c3524e776d4e3f0481627c090bcbb7387cd3b3a1
localmgr <localmgr@fengt1-stg4.rose-hulman.edu>
2020-01-30 16:11:31 -0500

    Connection done

---
+++ src/ConnectionService.java
@@ -0,0 +1,42 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+
+public class ConnectionService {
+	private Connection conn;
+	
+	public ConnectionService() {
+		conn = null;
+	}
+	public boolean connect(String user, String password) {
+		String connectionUrl = "jdbc:sqlserver://golem.csse.rose-hulman.edu:1433;"  
+				  + "database=SodaBasefengt120;"
+                  + "user=" + user + ";"
+                  + "password=" + password + ";"
+                  + "encrypt=false;"
+                  + "trustServerCertificate=true;"
+                  + "loginTimeout=30;";
+
+		try {
+			this.conn = DriverManager.getConnection(connectionUrl);
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public Connection getConnection() {
+		return this.conn;
+	}
+	
+	public void closeConnection() {
+		try {
+			this.conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,21 @@
+import java.sql.Connection;
+
 import javax.swing.JFrame;
+import java.sql.Connection;
 
 public class Main {
 
 	public static void main(String[] args) {
 		new RateMyClassMain();
+		ConnectionService connS = new ConnectionService();
+		if (connS.connect("fengt1", "Eagelbaba8")) {
+			Connection conn = connS.getConnection();
+			
+		}	
+		else {
+			System.out.println("Error");
+		}
+			
 	}
 
 }
